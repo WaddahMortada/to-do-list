937c4485ec83bac686cf7691c88dcd4c84dd0ef2
Waddah Mortada <w.mortada87@gmail.com>
2020-06-19 14:09:50 +0100

    Remove unused bootstrap

--- src/components/listContainer.js
+++ src/components/listContainer.js
@@ -2,19 +2,11 @@ import React from 'react'
 import List from './taskList'
 // eslint-disable-next-line no-unused-vars
 import styles from '../style/style.css'
-// import { Col, Card } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 function ListContainer (props) {
   console.log('ListContainer: props ', props)
   return (
-    // <Col>
-    //   <Card>
-    //     <Card.Body>
-    //       <TasksList callbackParent={props.callbackParent} taskList={props.taskList} currentTask={props.currentTask} />
-    //     </Card.Body>
-    //   </Card>
-    // </Col>
     <div className="listContainer">
       <List callbackParent={props.callbackParent} taskList={props.taskList} currentTask={props.currentTask} />
     </div>
--- src/components/taskContainer.js
+++ src/components/taskContainer.js
@@ -2,20 +2,12 @@ import React from 'react'
 import TaskContent from './taskContent'
 // eslint-disable-next-line no-unused-vars
 import styles from '../style/style.css'
-// import { Col, Card } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 function TaskContainer (props) {
   console.log('TaskConatiner: props ', props)
 
   return (
-    // <Col>
-    //   <Card>
-    //     <Card.Body>
-    //       <TaskContent />
-    //     </Card.Body>
-    //   </Card>
-    // </Col>
     <div className="taskContainer">
       <TaskContent callbackParent={props.callbackParent} task={props.task} />
     </div>
